3d8f2d3b4940e169d8318062dff2239539bcf215
Ignacio Rubio <stephanoignacio28@outlook.com>
2026-02-26 07:47:14 -0500

    Agregado el primer ejercicio del taller, un saludo formateado con f-strings

---
+++ 01_saludo_formateado.py
@@ -0,0 +1,19 @@
+"""
+EJERCICIO 1 — saludo formateado
+Archivo: 01_saludo_formateado.py
+
+Pedir:
+- nombre  
+- edad  
+- ciudad  
+
+Mostrar:
+Hola Juan, tienes 20 años y vives en Quito
+
+Usar:
+input, int, f-string  
+"""
+nombre = input("Introduce tu nombre: ")
+edad = int(input("Introduce tu edad: "))
+ciudad = input("Introduce tu ciudad: ")
+print(f"Hola {nombre}, tienes {edad} años y vives en {ciudad}")
